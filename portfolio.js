4d8ab19110843671aaf2d4c756ac7ee4636de440
Dakshesh Chauhan <chauhan06dakshesh@gmail.com>
2024-06-26 22:31:47 +0530

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// alert('Welcome! to my Portfolio page')
+
+function myFunction() {
+    document.getElementById('icon').style.display = 'none'
+    document.getElementById('hidden').style.display = 'none'
+    document.getElementById('nav-res-ext1').style.display = 'block'
+    document.getElementById('nav-res-ext2').style.display = 'block'
+    document.getElementById('nav-res-ext3').style.display = 'block'
+    document.getElementById('nav-res-ext4').style.display = 'block'
+}
+
